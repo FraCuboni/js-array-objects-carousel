3c99529c8774152b9f8e6b6f75feb71ed3685a39
FraCuboni <fracuboni@gmail.com>
2024-06-19 15:49:00 +0200

    feat basic css done

--- js/script.js
+++ js/script.js
@@ -1,81 +1,117 @@
+// Dato un array di oggetti letterali con: - url dell’immagine - titolo - descrizione
+// Creare un carosello come nella foto allegata.
+// Milestone 0:
+// Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l’immagine grande in modo da poter stilare lo slider.
+// Milestone 1:
+// Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.   Al click dell’utente sulle frecce verso sinistra o destra, l’immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+// Milestone 2:
+// Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è la prima e l’utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l’ultima e viceversa per l’ultima miniatura se l’utente clicca la freccia verso sinistra.
+// BONUS 1:
+// Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
+// BONUS 2:
+// Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
+// BONUS 3:
+// Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
+// Buon lavoro e buon divertimento!
 
-// MILESTONE 2
-// Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell’array fornito e un semplice ciclo for che concatena un template literal. Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile. Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
-// MILESTONE 3
-// Al click dell’utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
 
-// creo l'array immagini
-const imgArray = [
-    'img/01.webp',
-    'img/02.webp',
-    'img/03.webp',
-    'img/04.webp',
-    'img/05.webp'
-]
+// // creo l'array immagini
+// const imgArray = [
+//     'img/01.webp',
+//     'img/02.webp',
+//     'img/03.webp',
+//     'img/04.webp',
+//     'img/05.webp'
+// ]
 
-// collego l'elemento container
-const container = document.querySelector('.container');
+const images = [
+    { 
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.', },
+    { 
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    },
+    { 
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.", },
+    { 
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city', },
+    { 
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
 
-// creo il ciclo per prelevare gli elementi dall'array
-for(let i = 0; i < imgArray.length; i++){
-    let imgItem = imgArray[i];
-    console.log(imgItem);
+     } ];
 
-    // creo gli elementi box-img dove inserire l'immagine all'interno dell'elemento container
-    let boxImg = `
-        <div class="box-img">
-            <img src="${imgItem}" alt="">
-        </div>`;
+// // collego l'elemento container
+// const container = document.querySelector('.container');
 
-    // inserisco il div.box-img all'interno del div.container
-    container.innerHTML += boxImg;
+// // creo il ciclo per prelevare gli elementi dall'array
+// for(let i = 0; i < imgArray.length; i++){
+//     let imgItem = imgArray[i];
+//     console.log(imgItem);
 
-}
+//     // creo gli elementi box-img dove inserire l'immagine all'interno dell'elemento container
+//     let boxImg = `
+//         <div class="box-img">
+//             <img src="${imgItem}" alt="">
+//         </div>`;
 
-// seleziono gli elementi in pagina
-const boxes = document.getElementsByClassName('box-img');
-console.log(boxes);
+//     // inserisco il div.box-img all'interno del div.container
+//     container.innerHTML += boxImg;
 
-let activeImg = 0;
+// }
 
-boxes[activeImg].classList.add('active');
-console.log(boxes[0]);
+// // seleziono gli elementi in pagina
+// const boxes = document.getElementsByClassName('box-img');
+// console.log(boxes);
 
-// creo il button per attivare e disattivare le immagini
-const nextImg = document.querySelector('.next-img');
-const prevImg = document.querySelector('.prev-img');
+// let activeImg = 0;
 
-nextImg.addEventListener('click',
-    function(){
+// boxes[activeImg].classList.add('active');
+// console.log(boxes[0]);
 
-        // rimuovo il bug di fine immagine
-        if(activeImg < imgArray.length -1){
-        // cacello l'active dall'elemento attivo
-        boxes[activeImg].classList.remove('active');
-        // do il valore + 1 ad activeimg ogni click
-        activeImg = activeImg + 1;
+// // creo il button per attivare e disattivare le immagini
+// const nextImg = document.querySelector('.next-img');
+// const prevImg = document.querySelector('.prev-img');
 
-        // aggiungo classe active
-        boxes[activeImg].classList.add('active');
+// nextImg.addEventListener('click',
+//     function(){
 
-        }
-    }
-)
+//         // rimuovo il bug di fine immagine
+//         if(activeImg < imgArray.length -1){
+//         // cacello l'active dall'elemento attivo
+//         boxes[activeImg].classList.remove('active');
+//         // do il valore + 1 ad activeimg ogni click
+//         activeImg = activeImg + 1;
 
-prevImg.addEventListener('click',
-    function(){
+//         // aggiungo classe active
+//         boxes[activeImg].classList.add('active');
 
-        // rimuovo il bug di fine immagine
-        if(activeImg > 0){
-        // cacello l'active dall'elemento attivo
-        boxes[activeImg].classList.remove('active');
-        // do il valore - 1 ad activeimg ogni click
-        activeImg = activeImg - 1;
-        // aggiungo classe active
-        boxes[activeImg].classList.add('active');
+//         }
+//     }
+// )
 
-        }
-    }
-)
+// prevImg.addEventListener('click',
+//     function(){
+
+//         // rimuovo il bug di fine immagine
+//         if(activeImg > 0){
+//         // cacello l'active dall'elemento attivo
+//         boxes[activeImg].classList.remove('active');
+//         // do il valore - 1 ad activeimg ogni click
+//         activeImg = activeImg - 1;
+//         // aggiungo classe active
+//         boxes[activeImg].classList.add('active');
+
+//         }
+//     }
+// )
 
 
